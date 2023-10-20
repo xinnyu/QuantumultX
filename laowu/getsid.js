1cf4342802db85313ef02d004a6751dc67dde059
pxy <panxinyu@mgtv.com>
2023-10-20 09:59:21 +0800

    no msg

---
+++ laowu/getsid.js
@@ -0,0 +1,20 @@
+(async () => {
+    if (typeof $request != "undefined") {
+        // 解析URL
+        const parsedUrl = new URL($request.url);
+        // 获取sid参数的值
+        const sid = parsedUrl.searchParams.get("sid");
+        if (sid) {
+            // 保存sid
+            $prefs.setValueForKey(sid, "LW_MINIAPP_SID");
+            $notify("【老吴日单】", "SID", "获取SID成功: " + sid);
+        } else {
+            $notify("【老吴日单】", "SID", "获取SID失败: sid not found in the URL.");
+        }
+    } else {
+        $notify("【老吴日单】", "SID", "获取SID失败: request undefined.");
+    }
+})().finally(() => {
+    $.done();
+});
+
